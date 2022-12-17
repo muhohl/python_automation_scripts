94ed2c5af7d5017d9e54a5222bf5d5c495e4443f
muhohl <max.hohl@utas.edu.au>
2022-12-17 20:34:02 +1100

    Added cite_key.py

---
+++ cite_key.py
@@ -0,0 +1,59 @@
+'''
+Needs a little bit more testing. But I'm quiet optimistic that most of it should work.
+But the current algorithm is quiet slow, with a three fold nested for loop.
+'''
+
+import os
+import re
+
+# read file
+# Make this line interactive
+print("Enter .org file")
+file_name = input()
+file1 = open(file_name, "r")
+
+# load all my notes file names.
+# path = "~/Dropbox/Zettelkasten/notes/"
+path = os.getcwd()
+path = path+"/"
+dir_list = os.listdir(path)
+
+# transforms file into str object.
+textfile = file1.read()
+
+# finds all citations in the manuscrip.
+matches = re.findall("@\w+", textfile)
+
+# remove @ for matching with lit notes file names.
+for i in range(len(matches)):
+    matches[i] = matches[i].replace("@", "")
+
+print(path)
+# replaces citation with id from the lit notes.
+for i in range(len(matches)):
+    print(i)
+    for j in range(len(dir_list)):
+        if matches[i] in dir_list[j]:
+            roam_path = path+dir_list[j]
+            roam = open(roam_path)
+            textroam = roam.read()
+            splitroam = textroam.split()
+            author = "@"+matches[i]
+            for k in range(len(splitroam)):
+                if splitroam[k] == ":ID:":
+                    id_link = splitroam[k+1]
+            id_link = "[[id:"+id_link+"]["+matches[i]+"]]"
+            textfile = textfile.replace(author, str(id_link))
+
+# remove cite key otherwise link for doesn't work if it includes
+# citation that doesn't exist as a note yet.
+textfile = textfile.replace("cite:", "")
+
+# Save file as new org file
+# Look at write and read option. Ideally I create the
+# org-roam file first and add the textfile data after the
+# header information, with 'w' option this might already
+# work
+# Prompt used for name of output file
+new = open(file_name, "w")
+new.write(textfile)
